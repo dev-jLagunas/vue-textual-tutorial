a39bf650e9f58470585f48f7ac36a3124846bbbc
Juan Lagunas <dev.jlagunas@gmail.com>
2024-08-09 10:32:45 +0900

    Updates

---
+++ assets/DataBinding-B2mACLgX.js
@@ -0,0 +1,1 @@
+import{_ as e,o as n,c}from"./index-wgE79rxs.js";const t={};function a(o,r){return n(),c("h1",null,"Data Binding Vue")}const _=e(t,[["render",a]]);export{_ as default};
---
+++ assets/TemplateSyntax-C-yuHsqS.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as n}from"./index-wgE79rxs.js";const c={};function o(a,r){return t(),n("h1",null,"template syntax component here")}const _=e(c,[["render",o]]);export{_ as default};
